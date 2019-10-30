b30714851117d615c987704d56036b8e8eae9f49
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-10-30 15:56:44 +0300

    Renaming

--- src/scripts/http-service.js
+++ src/scripts/http-service.js
@@ -3,7 +3,7 @@ export class HttpService {
     #apiKey = '&apiKey=980e9d4359984b1bb923d5e1043ce9e2';
     #baseUrl = 'https://newsapi.org/';
 
-    async getArticles(arg) {
+    async getNewsData(arg) {
         const response = await fetch(`${this.#baseUrl}${arg}${this.#apiKey}`);
         if (response.ok) {
             const responseData = await response.json();
