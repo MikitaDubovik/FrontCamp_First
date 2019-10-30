304f96a7ef87d313d996e2e237fcd5606536f73b
Mikita Dubovik <Mikita_Dubovik@epam.com>
2019-10-30 20:23:28 +0300

    Added binding for eventListener

--- src/scripts/news-sources-list.js
+++ src/scripts/news-sources-list.js
@@ -5,9 +5,11 @@ import { NewsArticlesList } from './news-articles-list'
 export class NewsSourcesList {
 
     #httpService;
+    #newsArticlesList
 
     constructor() {
         this.#httpService = new HttpService();
+        this.#newsArticlesList = new NewsArticlesList();
     }
 
     async renderHeaders() {
@@ -18,15 +20,14 @@ export class NewsSourcesList {
         responseData.sources.forEach(element => {
             let item = document.createElement('li');
             item.id = element.id;
-            item.addEventListener("click", this.fetchArticles);
+            item.addEventListener("click", this.fetchArticles.bind(this));
             item.appendChild(document.createTextNode(element.name));
             list.appendChild(item);
         });
     }
 
-    //TODO: Не знаю как в EventListener пропихнуть приватное поле
-    async fetchArticles() {
-        const responseData = await new HttpService().getNewsData(`v1/articles?source=${this.id}`);
-        await new NewsArticlesList().renderArticles(responseData, 'articles');
+    async fetchArticles(event) {
+        const responseData = await this.#httpService.getNewsData(`v1/articles?source=${event.srcElement.id}`);
+        await this.#newsArticlesList.renderArticles(responseData, 'articles');
     }
 }
